cb8090f24a4d6a93c217eca019f6f3f5597fa483
TitanLi <Lisheng0706@gmail.com>
2017-07-28 14:59:16 +0800

    Add react ppt

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # reactjs_book
+[React 快速上手](https://drive.google.com/open?id=1ZC945U4l6EgM5SpzL3uJOTECw7aBpvZ1iQleEJSkNEA)
--- message/src/index.js
+++ message/src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myStyle = {
+  fontSize : 100,
+  color : '#00FFFF'
+}
+
+class Title extends React.Component{
+  render(){
+    return <h1>{this.props.title}</h1>
+  }
+}
+
+class Link extends React.Component{
+  render(){
+    return <a href={this.props.link}>{this.props.link}</a>
+  }
+}
+
+class Main extends React.Component{
+  render(){
+    return(
+      <div>
+        <Title title={this.props.title}/>
+        <Link link={this.props.link}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main title="Google" link="http://www.google.com"/>,document.getElementById('root'));
+
 registerServiceWorker();
